a03acdb4a138d0f8450f147a60c56961fe508f8f
danielinuth702 <daniels.uth702@gmail.com>
2018-07-12 14:59:40 +0800

    Initial commit

---
+++ spec/simple-vue-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SimpleVueView from '../lib/simple-vue-view';
+
+describe('SimpleVueView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
